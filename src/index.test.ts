d7bf027a3ccad9955f65c3e66d387107d9126c64
sadness_ojisan <sadness.ojisan@gmail.com>
2022-01-25 14:57:58 +0900

    Update tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,23 @@
 import { addNullable } from "./index";
 import simple from "../fixture/simple.json";
+import anyof from "../fixture/anyof.json";
+import anyofOptional from "../fixture/anyof-optional.json";
+import anyofOptionalResult from "../fixture/anyof-optional-result.json";
 
 describe("addNullable", () => {
   test("simple", () => {
-    console.log(simple);
     // @ts-expect-error
     const actual = addNullable(simple);
     expect(actual).toEqual(simple);
+  });
+  test("anyof", () => {
+    // @ts-expect-error
+    const actual = addNullable(anyof);
+    expect(actual).toEqual(anyof);
+  });
+  test("anyof optional", () => {
+    // @ts-expect-error
+    const actual = addNullable(anyofOptional);
+    expect(actual).toEqual(anyofOptionalResult);
   });
 });
